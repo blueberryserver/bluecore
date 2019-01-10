6af52cd97a19c2018131edf3c24b359cb16e2a42
blueberryserver <jugulle@gmail.com>
2019-01-10 12:31:29 +0900

    log directory create

--- bluelog.go
+++ bluelog.go
@@ -1,16 +1,35 @@
 package bluecore
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/blueberryserver/bluecore/bluecp"
 )
 
 // InitLog ...
 func InitLog(filename string) (*os.File, error) {
 	fileLog, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 	if err != nil {
-		return nil, err
+		filePathNameArry := strings.Split(filename, "/")
+		path := ""
+		for i := 0; i < len(filePathNameArry)-1; {
+			fmt.Println(filePathNameArry[i])
+			path += filePathNameArry[i]
+			i++
+		}
+		err := bluecp.MKDIR(path, 0666)
+		if err != nil {
+			return nil, err
+		}
+
+		fileLog, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+		if err != nil {
+			return nil, err
+		}
 	}
 	mutiWriter := io.MultiWriter(fileLog, os.Stdout)
 	log.SetOutput(mutiWriter)
